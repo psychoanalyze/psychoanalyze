172ca2cb987791e99845651612cbccacb3649a33
Tyler Schlichenmeyer <tyler.schlichenmeyer@gmail.com>
2022-04-22 04:13:26 -0500

    add curve transformation

--- psychoanalyze/__init__.py
+++ psychoanalyze/__init__.py
@@ -1,1 +1,13 @@
-__version__ = '0.1.0'
+import pandas as pd
+
+__version__ = "0.1.0"
+
+
+def fake() -> pd.DataFrame:
+    return pd.DataFrame(
+        {"Result": [0, 1], "x": [1, 1]}, index=pd.Index([1, 2], name="Trial")
+    )
+
+
+def curve(trials: pd.DataFrame):
+    return trials.groupby("x").mean()
--- tests/test_psychoanalyze.py
+++ tests/test_psychoanalyze.py
@@ -1,5 +1,25 @@
 from psychoanalyze import __version__
+import psychoanalyze as pa
+import pandas as pd
+import datatest as dt
+import pytest
+
+
+@pytest.fixture
+def fake_df():
+    return pd.DataFrame(
+        {"Result": [0, 1], "x": [1, 1]}, index=pd.Index([1, 2], name="Trial")
+    )
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
+
+
+def test_faker(fake_df):
+    assert pa.fake().equals(fake_df)
+
+
+def test_curve(fake_df):
+    df = pd.DataFrame(fake_df)
+    dt.validate(pa.curve(df), pd.Series([0.5], index=pd.Index([1], name="x")))
