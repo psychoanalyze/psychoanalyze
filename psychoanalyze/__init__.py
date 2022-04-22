37917806ef5c84b99a06e8f762f78d784be57a5d
Tyler Schlichenmeyer <tyler.schlichenmeyer@gmail.com>
2022-04-22 18:49:11 -0500

    implement basic psycometric function psi using expit

--- psychoanalyze/__init__.py
+++ psychoanalyze/__init__.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import numpy as np
+from scipy.special import expit
+import psychoanalyze as pa
 
 __version__ = "0.1.0"
 
@@ -9,5 +12,15 @@ def fake() -> pd.DataFrame:
     )
 
 
-def curve(trials: pd.DataFrame):
+def curve(trials: pd.DataFrame) -> pd.Series:
     return trials.groupby("x").mean()
+
+
+def weber_coefficient(curves: pd.DataFrame) -> float:
+    return 1
+
+
+def psi() -> pd.Series:
+    expected_x = np.linspace(-3, 3)
+    expected_y = expit(expected_x)
+    return pd.Series(expected_y, index=expected_x)
--- tests/test_psychoanalyze.py
+++ tests/test_psychoanalyze.py
@@ -3,10 +3,12 @@ import psychoanalyze as pa
 import pandas as pd
 import datatest as dt
 import pytest
+import numpy as np
+from scipy.special import expit
 
 
 @pytest.fixture
-def fake_df():
+def trials() -> pd.DataFrame:
     return pd.DataFrame(
         {"Result": [0, 1], "x": [1, 1]}, index=pd.Index([1, 2], name="Trial")
     )
@@ -16,10 +18,24 @@ def test_version():
     assert __version__ == "0.1.0"
 
 
-def test_faker(fake_df):
-    assert pa.fake().equals(fake_df)
+def test_faker(trials: pd.DataFrame):
+    assert pa.fake().equals(trials)
 
 
-def test_curve(fake_df):
-    df = pd.DataFrame(fake_df)
+def test_curve(trials: pd.DataFrame):
+    df = pd.DataFrame(trials)
     dt.validate(pa.curve(df), pd.Series([0.5], index=pd.Index([1], name="x")))
+
+
+def test_weber():
+    curves = pd.DataFrame(
+        {"Difference Threshold": [0, 1], "Base Stimulus Intensity": [0, 1]},
+        index=pd.Index([1, 2], name="Trial"),
+    )
+    assert pa.weber_coefficient(curves) == 1
+
+
+def test_psi():
+    expected_x = np.linspace(-3, 3)
+    expected_y = expit(expected_x)
+    dt.validate(pa.psi(), pd.Series(expected_y, index=expected_x))
