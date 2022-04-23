50e17f44941f6f9034262d31c3f2f926f5213f18
Tyler Schlichenmeyer <tyler.schlichenmeyer@gmail.com>
2022-04-23 18:17:12 -0500

    remove doc auto generator for now

--- gen_ref_pages.py
+++
@@ -1,7 +0,0 @@
-"""Generate the code reference pages."""
-
-from pathlib import Path
-import mkdocs_gen_files
-
-with mkdocs_gen_files.open(Path("API.md"), "w") as fd:
-    print("::: psychoanalyze", file=fd)
