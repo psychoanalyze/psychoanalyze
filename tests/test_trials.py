a2ff7bddab9324177051e5dd37704d81f6f873ee
Tyler Schlichenmeyer <tyler.schlichenmeyer@gmail.com>
2022-06-15 03:47:05 -0500

    remove helper test function

--- tests/test_trials.py
+++ tests/test_trials.py
@@ -13,19 +13,5 @@ def X():
 
 
 def test_generate():
-    assert len(pa.trial.generate(100)) == 100
-
-
-@pytest.mark.parametrize("n_trials", [10, 11])
-def test_generate_n_trials(n_trials, subjects, X):
-    n_sessions = 10
-    data = pa.data.generate(
-        subjects=subjects,
-        n_sessions=n_sessions,
-        y="Threshold",
-        n_trials_per_stim_level=n_trials,
-        X=X,
-    )
-    assert "Threshold" in set(data.columns)
-    assert len(data) == n_sessions * len(subjects) * 8
-    assert all(data["n"] == n_trials)
+    data = pa.trial.generate(100)
+    assert len(data) == 100
