1a72a9f827d4cfbc0a1746905c266ad40cca9996
Ty Schlichenmeyer <tyschlich@gmail.com>
2022-11-12 09:02:21 +0000

    more realistic example values

--- dashboard/simulate_sd.py
+++ dashboard/simulate_sd.py
@@ -17,12 +17,16 @@ app.layout = dbc.Container(
                         figure=px.line(
                             pd.DataFrame(
                                 {
-                                    "Fixed Pulse Width (μs)": [1, 2, 3, 4, 5],
-                                    "Threshold Amplitude (μA)": [5, 4, 3, 2, 1],
+                                    "Fixed Pulse Width (μs)": list(
+                                        range(100, 600, 100)
+                                    ),
+                                    "Threshold Amplitude (μA)": list(
+                                        range(1000, 0, -200)
+                                    ),
                                     "Monkey": ["U"] * 2 + ["Y"] * 3,
                                     "Day": [1, 1, 2, 2, 2],
-                                    "error_y": [0.5, 0.5, 0.5, 0.5, 0.5],
-                                    "error_y_minus": [0.5, 0.5, 0.5, 0.5, 0.5],
+                                    "error_y": [50] * 5,
+                                    "error_y_minus": [50] * 5,
                                 }
                             ),
                             x="Fixed Pulse Width (μs)",
@@ -40,12 +44,14 @@ app.layout = dbc.Container(
                         figure=px.line(
                             pd.DataFrame(
                                 {
-                                    "Fixed Amplitude (μA)": [1, 2, 3, 4, 5],
-                                    "Threshold Pulse Width (μs)": [5, 4, 3, 2, 1],
+                                    "Fixed Amplitude (μA)": list(range(100, 1100, 200)),
+                                    "Threshold Pulse Width (μs)": list(
+                                        range(600, 100, -100)
+                                    ),
                                     "Monkey": ["U"] * 2 + ["Y"] * 3,
                                     "Day": [1, 1, 2, 2, 2],
-                                    "error_y": [0.5, 0.5, 0.5, 0.5, 0.5],
-                                    "error_y_minus": [0.5, 0.5, 0.5, 0.5, 0.5],
+                                    "error_y": [50] * 5,
+                                    "error_y_minus": [50] * 5,
                                 }
                             ),
                             x="Fixed Amplitude (μA)",
@@ -67,12 +73,12 @@ app.layout = dbc.Container(
                         figure=px.line(
                             pd.DataFrame(
                                 {
-                                    "Pulse Width (μs)": [1, 2, 3, 4, 5],
-                                    "Threshold Charge (nC)": [1, 2, 3, 4, 5],
+                                    "Pulse Width (μs)": list(range(50, 500, 100)),
+                                    "Threshold Charge (nC)": list(range(50, 500, 100)),
                                     "Dimension": ["Amp"] * 2 + ["Width"] * 3,
                                     "Day": [1, 1, 2, 2, 2],
-                                    "error_y": [0.5, 0.5, 0.5, 0.5, 0.5],
-                                    "error_y_minus": [0.5, 0.5, 0.5, 0.5, 0.5],
+                                    "error_y": [50] * 5,
+                                    "error_y_minus": [50] * 5,
                                 }
                             ),
                             x="Pulse Width (μs)",
@@ -90,12 +96,12 @@ app.layout = dbc.Container(
                         figure=px.line(
                             pd.DataFrame(
                                 {
-                                    "Pulse Width (μs)": [1, 2, 3, 4, 5],
-                                    "Threshold Charge (nC)": [1, 2, 3, 4, 5],
+                                    "Pulse Width (μs)": list(range(50, 500, 100)),
+                                    "Threshold Charge (nC)": list(range(5, 50, 10)),
                                     "Dimension": ["Amp"] * 2 + ["Width"] * 3,
                                     "Day": [1, 1, 2, 2, 2],
-                                    "error_y": [0.5, 0.5, 0.5, 0.5, 0.5],
-                                    "error_y_minus": [0.5, 0.5, 0.5, 0.5, 0.5],
+                                    "error_y": [5] * 5,
+                                    "error_y_minus": [5] * 5,
                                 }
                             ),
                             x="Pulse Width (μs)",
