d6e01bfae98bbb40af9bd8a3823ac87c3deb874a
Ty Schlichenmeyer <tyschlich@gmail.com>
2022-11-12 06:53:33 +0000

    add ecdf simulation

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -134,7 +134,13 @@ def test_psychometric_function():
     assert len(psychometric_plot.data) == 1
 
 
-def test_ecdf():
+def test_ecdf_location_no_data():
     blocks = pd.DataFrame({"location": []})
     ecdf_fig = pa.plot.ecdf(blocks, "location")
     assert ecdf_fig.layout.xaxis.title.text == "location"
+
+
+def test_ecdf_width_no_data():
+    blocks = pd.DataFrame({"width": []})
+    ecdf_fig = pa.plot.ecdf(blocks, "width")
+    assert ecdf_fig.layout.xaxis.title.text == "width"
