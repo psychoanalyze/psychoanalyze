78202469487056494caed1dcdf683452de8b5e91
Ty Schlichenmeyer <tyschlich@gmail.com>
2023-04-07 03:56:26 +0000

    feat: pa.simulate.run

--- psychoanalyze/simulate.py
+++ psychoanalyze/simulate.py
@@ -1,6 +1,10 @@
+import random
 import pandas as pd
 
 
-def run(n: int) -> pd.Series:
+def run(n_trials: int) -> pd.Series:
     """Run a simulation"""
-    return pd.Series([])
+    return pd.Series(
+        [random.random() for _ in range(n_trials)],
+        index=pd.Index(list(range(n_trials)), name="TrialID"),
+    )
