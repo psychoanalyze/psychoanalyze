f8c57bcfca900213afab3ba09d4ed8bc2f808f20
Tyler Schlichenmeyer <tyler.schlichenmeyer@gmail.com>
2023-07-06 01:51:30 +0000

    docs: add docstrings for test module

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,11 @@
-"""Test suite for Psychoanalyze."""
+"""Automated tests for `psychoanalyze`.
+
+Tests are documented for both users of and contributors to the package.
+
+- *Users* should reference tests as examples for efficient
+utilization of the `psychoanalyze` package.
+- *Contributors* should reference tests to assist in writing the required tests
+for accepted contributions.
+
+Generally, each test file corresponds to its corresponding submodule.
+"""
