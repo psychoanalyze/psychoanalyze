3b7fd44d18949e36496ae6d2e59884d7687669de
Tyler Schlichenmeyer <tyler.schlichenmeyer@gmail.com>
2023-07-06 21:40:14 +0000

    docs: dashboard module docstring

--- dashboard/__init__.py
+++ dashboard/__init__.py
@@ -1,1 +1,10 @@
-"""Dashboard for psychophysics data analysis."""
+"""The PsychoAnalyze dashboard is powered by Plotly/Dash.
+
+- `app.py` is the main file which contains the infrastructure for the Dash app.
+and callback functions in their entirety.
+
+- `layout.py` specifies the layout of the HTML and Dash components that comprise
+the app.
+
+- `components.py` contains more complex and/or reusable components used in the app.
+"""
