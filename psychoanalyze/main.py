4e1391bcd07427ae918569825af5149f5ee68347
Ty Schlichenmeyer <ty.schlich@gmail.com>
2023-07-24 07:25:52 +0000

    feat: CLI commands! Just --version for now.

---
+++ psychoanalyze/main.py
@@ -0,0 +1,30 @@
+# Copyright 2023 Tyler Schlichenmeyer
+
+# This file is part of PsychoAnalyze.
+# PsychoAnalyze is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software Foundation,
+# either version 3 of the License, or (at your option) any later version.
+
+# PsychoAnalyze is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along with
+# PsychoAnalyze. If not, see <https://www.gnu.org/licenses/>.
+
+"""PsychoAnalyze command line interface."""
+
+import importlib.metadata
+
+import typer
+from rich.console import Console
+
+
+def main(command: str = "version") -> None:
+    """Main commands."""
+    if command == "version":
+        Console().print(importlib.metadata.version("psychoanalyze"))
+
+
+if __name__ == "__main__":
+    typer.run(main)
